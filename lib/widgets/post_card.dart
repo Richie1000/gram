c0e2d38fb181aa164c54e8a8bd12bf89a329f1c4
Richie <esselricmond78@gmail.com>
2022-09-04 12:14:23 +0000

    adding feed v2

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -10,15 +10,16 @@ class PostCard extends StatelessWidget {
     return Container(
       color: mobileBackgroundColor,
       padding: EdgeInsets.symmetric(vertical: 10),
-      child: Column(children: [
-        Container(
+      child: Column(
+        children: [
+          Container(
             padding: EdgeInsets.symmetric(vertical: 4, horizontal: 15)
                 .copyWith(right: 0),
             child: Row(
               children: [
                 CircleAvatar(
                   radius: 16,
-                  backgroundImage: AssetImage("assets/defaultPic.jpg"),
+                  backgroundImage: AssetImage("assets/defaultpic.jpg"),
                 ),
                 Expanded(
                   child: Padding(
@@ -36,13 +37,109 @@ class PostCard extends StatelessWidget {
                 ),
                 IconButton(
                   onPressed: () {
-                    //showDialog(context: context, builder: builder)
+                    showDialog(
+                      context: context,
+                      builder: (context) => Dialog(
+                        child: ListView(
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                          shrinkWrap: true,
+                          children: ["Delete"]
+                              .map(
+                                (e) => InkWell(
+                                  onTap: () {},
+                                  child: Container(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 12, horizontal: 16),
+                                    child: Text(e),
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      ),
+                    );
                   },
                   icon: Icon(Icons.more_vert),
                 ),
               ],
-            ))
-      ]),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.35,
+            width: double.infinity,
+            child: Image.network(
+              "https://images.unsplash.com/photo-1571210862729-78a52d3779a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          //Like Comment Section
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.favorite),
+                color: Colors.red,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.message),
+                //color: Colors.red,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.share),
+                //color: Colors.red,
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.bookmark_added_outlined)),
+                ),
+              ),
+            ],
+          ),
+          //Caption and comment section
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                DefaultTextStyle(
+                  style: Theme.of(context)
+                      .textTheme
+                      .subtitle2!
+                      .copyWith(fontWeight: FontWeight.w800),
+                  child: Text(
+                    "142 likes",
+                    style: Theme.of(context).textTheme.bodyText2,
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(8),
+                  child: RichText(
+                    text: TextSpan(
+                        style: const TextStyle(color: primaryColor),
+                        children: [
+                          TextSpan(
+                              text: 'Username',
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold)),
+                          TextSpan(
+                            text: '    Caption this',
+                          ),
+                        ]),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
